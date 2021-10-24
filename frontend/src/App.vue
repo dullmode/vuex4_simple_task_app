878c76e4907d62665a0c874fe0b913a49f52a72a
dullmatian <someone_6262@yahoo.co.jp>
2021-10-24 14:56:24 +0900

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>To-Do List</h1>
+    <div>
+      <TodoNew />
+      <TodoList />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoNew from "@/components/ToDoNew.vue";
+import TodoList from './components/ToDoList.vue';
+
+export default {
+  components: {
+    TodoNew,
+    TodoList
+  }
+};
+</script>
---
+++ frontend/src/components/ToDoList.vue
@@ -0,0 +1,20 @@
+<template>
+<ul>
+  <li
+    v-for="todo in todos"
+    :key="todo.id"
+  >
+    {{ todo.task }}
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  computed: {
+    todos() {
+      return this.$store.getters.getTodos;
+    }
+  }
+};
+</script>
